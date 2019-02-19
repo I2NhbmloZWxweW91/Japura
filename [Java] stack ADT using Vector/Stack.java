2388edaeff9a7fd645dff7907a207d26f5fc4f7b
Caretaker  ^_^ <akiladananjaya79@gmail.com>
2019-02-19 11:25:31 +0530

    [Java] stack ADT using Vector

---
+++ [Java] stack ADT using Vector/Stack.java
@@ -0,0 +1,41 @@
+/*
+A simple implementation for `stack` DS using Vector in Java
+*/
+
+/* imports */
+import java.util.Vector;
+
+
+public class Stack<Object>{
+    
+    Vector stack_vector;
+    
+    public Stack(){
+        this.stack_vector = new Vector();
+    }
+    
+    
+    public void push(Object obj){
+        this.stack_vector.add(obj);
+    }
+    
+    
+    public Object peek(){
+        Object last_obj = (Object) this.stack_vector.get(this.stack_vector.size() - 1);
+        return (last_obj);
+    }
+    
+    
+    public Object pop(){
+        Object last_obj  = (Object) peek();
+        this.stack_vector.remove(this.stack_vector.size() - 1);
+        return (last_obj);
+    }
+    
+    
+    public boolean empty(){
+        if (this.stack_vector.isEmpty()) return true;
+        return false;
+    }
+    
+}
