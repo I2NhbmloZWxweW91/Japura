f2e8fb600b3c6fde3c87323ceb516d04c07fd9f1
Caretaker  ^_^ <akiladananjaya79@gmail.com>
2019-03-11 21:51:12 +0530

    src added

---
+++ [Java] Singly Linked List/Main.java
@@ -0,0 +1,44 @@
+package pkg907lab05;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        LinkedList ld = new LinkedList(10.5);
+        
+        ld.addToFront(12.6);
+        ld.addToEnd(22.33);
+        ld.addToEnd(333.33);
+        ld.addToFront(111.6);
+        
+        ld.print_all();
+        System.out.println("- -- - -- - -");
+        
+        int res = ld.addBefore(10.5, 99.99);
+        
+        ld.print_all();
+        System.out.println("- -- - -- - -");
+        
+        res = ld.addAfter(22.33, 888.88);
+        
+        ld.print_all();
+        System.out.println("- -- - -- - -");
+        
+        ld.deleteFront();
+        
+        ld.print_all();
+        System.out.println("- -- - -- - -");
+        
+        ld.deleteEnd();
+        ld.print_all();
+        System.out.println("- -- - -- - -");
+        
+        ld.addToEnd(44.5);
+        ld.addToEnd(3435.45);
+        ld.print_all();
+        System.out.println("- -- - -- - -");
+    }
+    
+    
+}
---
+++ [Java] Singly Linked List/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    
+    double value;
+    Node next;
+
+    public Node(double value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+      
+}
